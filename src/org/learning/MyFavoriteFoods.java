0db4465d7e424c1708c3266f14de8aa205142a37
Salvatore Bono <salvatorebono2001@gmail.com>
2024-03-27 14:54:10 +0100

    finish second ex

--- src/org/learning/MyFavoriteFoods.java
+++ src/org/learning/MyFavoriteFoods.java
@@ -1,7 +1,19 @@
 package org.learning;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MyFavoriteFoods {
     public static void main(String[] args) {
-
+        Scanner keyboard = new Scanner(System.in);
+        String[] food = new String[5];
+        System.out.println("Dimmi " + food.length + " cibi preferiti");
+        for (int i = 0; i < food.length; i++) {
+            food[i] = keyboard.nextLine();
+        }
+        System.out.println("Il tuo cibo preferito è: " + food[0]);
+        System.out.println("Il tuo ultimo cibo preferito è: " + food[4]);
+        System.out.println("Il cibo di mezzo è: " + food[2]);
+        keyboard.close();
     }
 }
