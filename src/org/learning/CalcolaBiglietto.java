9bc69d292a4586f77e2d4af2fb3e34764a9bbf34
Salvatore Bono <salvatorebono2001@gmail.com>
2024-03-27 14:19:09 +0100

    finish first ex

--- src/org/learning/CalcolaBiglietto.java
+++ src/org/learning/CalcolaBiglietto.java
@@ -1,7 +1,26 @@
 package org.learning;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
     public static void main(String[] args) {
-
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("How many kilometers do you want to travel?");
+        int km = keyboard.nextInt();
+        double totalPrice = km * 0.21;
+        System.out.print("How old are you?");
+        int age = keyboard.nextInt();
+       if (age < 18){
+            double discountUnder18 = totalPrice * 20 / 100 ;
+            double priceWithDiscount = totalPrice - discountUnder18 ;
+           System.out.println("il tuo biglietto costa " + priceWithDiscount + "€");
+       } else if (age > 65) {
+           double discountOver65 = totalPrice * 40 / 100 ;
+           double priceWithDiscount = totalPrice - discountOver65 ;
+           System.out.println("il tuo biglietto costa " + priceWithDiscount + "€");
+       }else {
+           System.out.println("il tuo biglietto costa " + totalPrice + "€");
+       }
+        keyboard.close();
     }
 }
---
+++ src/org/learning/MyFavoriteFoods.java
@@ -0,0 +1,7 @@
+package org.learning;
+
+public class MyFavoriteFoods {
+    public static void main(String[] args) {
+
+    }
+}
